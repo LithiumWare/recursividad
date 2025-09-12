5456d81726da49672ab0996d3090fdfb5955e7ab
Liet <cxxmills@outlook.com>
2025-09-12 15:54:34 -0500

    Update sumas PotenciaRecursiva.java

--- src/main/java/com/example/PotenciaRecursiva.java
+++ src/main/java/com/example/PotenciaRecursiva.java
@@ -1,8 +1,16 @@
 package com.example;
 
 public class PotenciaRecursiva {
+       
+    public static int multiplicar(int a, int b) {
+        if (b == 0) return 0;
+        return a + multiplicar(a, b - 1);
+    }
+
+    
     public static int potencia(int base, int exponente) {
         if (exponente == 0) return 1;
-        return base * potencia(base, exponente - 1);
+        return multiplicar(base, potencia(base, exponente - 1));
     }
 }
+
