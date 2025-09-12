50b3c3a12afbc0d2fdef013ea83ebf65d965caeb
Liet <cxxmills@outlook.com>
2025-09-12 16:05:31 -0500

    Update PotenciaRecursiva.java

--- src/main/java/com/example/PotenciaRecursiva.java
+++ src/main/java/com/example/PotenciaRecursiva.java
@@ -1,17 +1,25 @@
 package com.example;
 
 public class PotenciaRecursiva {
-       
-    public static double multiplicar(int a, int b) {
-        if (b == 0) return 0;
-        return a + multiplicar(a, b - 1);
-    }
 
-    
-    public static double potencia(int base, int exponente) {
+       public class PotenciaRecursiva {
+    public static int potencia(int base, int exponente) {
         if (exponente == 0) return 1;
-        return multiplicar(base, potencia(base, exponente - 1));
+        return base * potencia(base, exponente - 1);
     }
 }
+       
+   // public static double multiplicar(int a, int b) {
+     //   if (b == 0) return 0;
+     //   return a + multiplicar(a, b - 1);
+  //  }
+
+    
+  //  public static double potencia(int base, int exponente) {
+  //      if (exponente == 0) return 1;
+      //  return multiplicar(base, potencia(base, exponente - 1));
+   //}
+}
+
 
 
