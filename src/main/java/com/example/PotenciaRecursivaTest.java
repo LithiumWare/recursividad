eab7262a65aa1c51739e94fc64f025ce6cd7f3ea
Liet <cxxmills@outlook.com>
2025-09-11 23:08:18 -0500

    Update PotenciaRecursivaTest.java

--- src/main/java/com/example/PotenciaRecursivaTest.java
+++ src/main/java/com/example/PotenciaRecursivaTest.java
@@ -5,12 +5,39 @@ import org.junit.jupiter.api.Test;
 
 public class PotenciaRecursivaTest {
 
+    public class PotenciaRecursivaTest {
+
     @Test
-    void testPotencia() {
+    void testPotenciaPositiva() {
         assertEquals(8, PotenciaRecursiva.potencia(2, 3));
+    }
+
+    @Test
+    void testPotenciaExponenteCero() {
         assertEquals(1, PotenciaRecursiva.potencia(5, 0));
+    }
+
+    @Test
+    void testPotenciaGrande() {
         assertEquals(15625, PotenciaRecursiva.potencia(5, 6));
+    }
+
+    @Test
+    void testPotenciaCuatro() {
         assertEquals(81, PotenciaRecursiva.potencia(6, 4));
-        assertEquals(3, PotenciaRecursiva.potencia(3, 3));
+    }
+
+    @Test
+    void testPotenciaNegativaBase() {
+        assertEquals(-27, PotenciaRecursiva.potencia(-3, 3));
+    }
+
+    @Test
+    void testPotenciaExponenteNegativo() {
+        assertEquals(0.03703, PotenciaRecursiva.potencia(3, -3), 0.0001); 
+       //tolerancia
     }
 }
+
+}
+
